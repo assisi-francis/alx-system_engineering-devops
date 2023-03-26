8f13133290a29dde6d733aa3ff4d6eeb76e7f058
assisi-francis <doncyco123@gmail.com>
2023-03-26 23:49:26 +0100

    add

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,7 +3,8 @@ file { '/home/ubuntu/.ssh/config':
   ensure  => file,
   mode    => '0600',
   owner   => 'ubuntu',
-  group   => 'ubuntu',
+  group   => 'ubuntu'i,
+  content => '
     Host 3.85.177.232
       IdentityFile ~/.ssh/school
       PasswordAuthentication no
